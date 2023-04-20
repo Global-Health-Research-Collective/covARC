81d968dc81cbbfde53f4c967d02b28dc8489ad71
nshreyasvi <shreyasvi.natraj@etu.unige.ch>
2023-04-20 13:02:13 +0200

    v1.51

--- pre_proc.py
+++ pre_proc.py
@@ -11,15 +11,17 @@ import gc
 
 list_ = []
 
-fields = ['CountryName','CountryCode','RegionName','RegionCode','Date','ConfirmedCases','ConfirmedDeaths','PopulationVaccinated']
+fields = ['iso_code','continent','location','date','total_cases','new_cases','total_deaths','new_deaths']
+#fields = ['CountryName','CountryCode','RegionName','RegionCode','Date','ConfirmedCases','ConfirmedDeaths','PopulationVaccinated']
 
-df = pd.read_csv('https://raw.githubusercontent.com/OxCGRT/covid-policy-tracker/master/data/OxCGRT_nat_latest_combined.csv', usecols = fields)
+df = pd.read_csv('https://covid.ourworldindata.org/data/owid-covid-data.csv', usecols = fields)
+#df = pd.read_csv('https://raw.githubusercontent.com/OxCGRT/covid-policy-tracker/master/data/OxCGRT_nat_latest_combined.csv', usecols = fields)
 #df = pd.read_csv('OxCGRT_nat_latest_combined.csv', usecols = fields)
 
-countries = df['CountryName'].unique()
+countries = df['location'].unique()
 
 for i in range(0,len(countries)):
-        df_temp = df.loc[df['CountryName']==countries[i]]
+        df_temp = df.loc[df['location']==countries[i]]
         print(df_temp.head())
-        df_temp.to_csv(os.path.join('ox_filtered/',str(countries[i])+'.csv'), index=False)
+        df_temp.to_csv(os.path.join('new_filtered/',str(countries[i])+'.csv'), index=False)
         gc.collect()
